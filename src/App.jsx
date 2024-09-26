f853e8943e4784d9476b7f173b4a59f7cdcc71d9
aniket <a.fadale@angularminds.in>
2024-09-27 03:44:42 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NoFoundPage from "./pages/NoFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+
+      <Route index element={<HomePage/>} />
+      <Route path='/jobs' element={<JobsPage/>} />
+      <Route path="*" element={<NoFoundPage/>}/>
+    </Route>
+)
+)
+const App = () => {
+  return <RouterProvider router={router}/>
+};
+
+export default App;
