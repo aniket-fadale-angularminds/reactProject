590f8462dea8ce330074a98eab7afceb0836b1f9
aniket <a.fadale@angularminds.in>
2024-09-30 01:20:02 +0530

    completed curd with rest api

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,56 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
-
+import AddJobPage from "./pages/AddJobPage";
 import NoFoundPage from "./pages/NoFoundPage";
+import EditJobPage from "./pages/EditJobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
 
-      <Route index element={<HomePage/>} />
-      <Route path='/jobs' element={<JobsPage/>} />
-      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
-      <Route path="*" element={<NoFoundPage/>}/>
-    </Route>
-)
-)
 const App = () => {
+
+
+const addJob=async (newJob)=>{
+  // console.log(newJob);
+  const res =  await fetch('/api/jobs',{
+    method:'POST',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(newJob),
+  });
+  return;
+  }
+
+const deleteJob = async(id)=>{
+  const res =  await fetch(`/api/jobs/${id}`,{
+    method:'DELETE',
+  });
+  return;
+ }
+ const updateJob = async(job)=>{
+  const res =  await fetch(`/api/jobs/${job.id}`,{
+    method:'PUT',
+    headers:{
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(job),
+  });
+  return;
+ }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+  
+        <Route index element={<HomePage/>} />
+        <Route path='/jobs' element={<JobsPage/>} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader}  />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit= {addJob} />}/>
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/> } loader={jobLoader} />
+        <Route path="*" element={<NoFoundPage/>}/>
+      </Route>
+  )
+  )
   return <RouterProvider router={router}/>
 };
 
